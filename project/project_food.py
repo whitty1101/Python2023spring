f8a3928bb51af62db86cb722a741be86bb8c44a4
whitty1101 <bagelcatboy@gmail.com>
2023-05-07 17:10:46 +0800

    20230507

---
+++ project/project_food.py
@@ -0,0 +1,14 @@
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from pathlib import Path
+import smtplib
+from tkinter import *
+import tkinter.ttk as ttk
+from tkscrolledframe import ScrolledFrame
+from tkinter import filedialog
+from PIL import Image, ImageTk
+from tkinter import messagebox
+
+food=Tk()
+
